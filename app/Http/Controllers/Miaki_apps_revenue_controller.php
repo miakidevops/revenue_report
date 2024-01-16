8df565a494771b4fad929d5e49532fa948cda74f
Md. Sohel Akter <sohelrakib929@gmail.com>
2024-01-16 12:23:53 +0600

    upd

--- app/Http/Controllers/Miaki_apps_revenue_controller.php
+++ app/Http/Controllers/Miaki_apps_revenue_controller.php
@@ -639,18 +639,18 @@ class Miaki_apps_revenue_controller extends Controller
     	$all_data = TargetRevenue::orderBy('year','desc')->orderBy('month','desc')->get();
     	// dd($all_data);
     	$month_name = array(
-    		1 => "January",
-    		2 => "February",
-    		3 => "March",
-    		4 => "April",
-    		5 => "May",
-    		6 => "June",
-    		7 => "July",
-    		8 => "August",
-    		9 => "September",
-    		10 => "October",
-    		11 => "November",
-    		12 => "December"
+    		"01" => "January",
+    		"02" => "February",
+    		"03" => "March",
+    		"04" => "April",
+    		"05" => "May",
+    		"06" => "June",
+    		"07" => "July",
+    		"08" => "August",
+    		"09" => "September",
+    		"10" => "October",
+    		"11" => "November",
+    		"12" => "December"
     	);
     	// dd($month_name);
 
